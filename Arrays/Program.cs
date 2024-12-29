30ad86cdfc0beee9a91fe66f268f6a80fb63a195
junho@bobsweep.com <shinjuno123@naver.com>
2024-12-29 15:14:32 -0800

    Add progressfrom freecodecamp

---
+++ Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int num3 = 15;
+            // angles?
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter a number: ");
+                numbers[i] = int.TryParse(Console.ReadLine(), out numbers[i])? numbers[i] : 0;
+            }         
+
+          
+            //Console.WriteLine($"{num1} {num2} {num3}");
+            //Console.WriteLine($"{numbers[0]} {numbers[1]} {numbers[2]}");
+
+
+            for (int i = 0; i < numbers.Length; i++) {
+                Console.Write($"{numbers[i]} ");   
+            }
+
+            Console.WriteLine();
+            
+            foreach (int num in numbers) {
+                Console.Write($"{num} ");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sort/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                1, 100, 20, 40, 50,10, 20, 30
+            };
+
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            foreach (int number in numbers) {
+                Console.Write($"{number} ");
+            }
+    
+            Console.ReadLine();
+        }
+    }
+}
